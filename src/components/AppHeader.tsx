21d7dec4da3e787190e48f24acf5a4d0520e39cf
jjeffrin <jjeffrin@gmail.com>
2022-07-03 21:20:00 +0530

    display version on app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,21 +1,25 @@
 import { Box, Heading, HStack, IconButton, Text, useColorMode } from "@chakra-ui/react"
 import { FaSun, FaMoon } from 'react-icons/fa'
+import packageInfo from '../../package.json'
 
 export const AppHeader = () => {
 
     const { colorMode, toggleColorMode } = useColorMode()
 
     return (
-        <HStack display={'flex'} flexDir={'row'} justifyContent={'space-between'} alignItems={'center'}>
-            <Box>
-                <Heading>ideas. 💡</Heading>
-                <Text>{'ideas >>> actual work ;)'}</Text>
-            </Box>
-            <IconButton
-                onClick={() => toggleColorMode()}
-                size={'sm'}
-                aria-label="Toggle dark/light mode"
-                icon={colorMode === 'dark' ? <FaSun /> : <FaMoon />} />
-        </HStack>
+        <>
+            <HStack display={'flex'} flexDir={'row'} justifyContent={'space-between'} alignItems={'center'}>
+                <Box>
+                    <Heading>ideas. 💡</Heading>
+                    <Text>{'ideas >>> actual work ;)'}</Text>
+                </Box>
+                <IconButton
+                    onClick={() => toggleColorMode()}
+                    size={'sm'}
+                    aria-label="Toggle dark/light mode"
+                    icon={colorMode === 'dark' ? <FaSun /> : <FaMoon />} />
+            </HStack>
+            <Text fontSize={'xs'}>v{packageInfo.version}</Text>
+        </>
     )
 }
