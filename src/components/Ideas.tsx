9203f4cfe73a12d8a8010439616178780ee66931
jjeffrin <jjeffrin@gmail.com>
2022-07-04 16:58:31 +0530

    added sorting feature

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -1,3 +1,4 @@
+import { Select } from "@chakra-ui/react"
 import { query, collection, orderBy, onSnapshot } from "@firebase/firestore"
 import { useEffect, useState } from "react"
 import { db } from "../FirebaseConfig"
@@ -7,19 +8,26 @@ import { Idea } from "./Idea"
 export const Ideas = () => {
 
     const [ideas, setIdeas] = useState<IdeaModel[]>([]);
+    const [sortBy, setSortBy] = useState<string>("createdDate");
 
     useEffect(() => {
-        const q = query(collection(db, `ideas`), orderBy("votes", "desc"))
+        // const q = query(collection(db, `ideas`), orderBy("votes", "desc"))
+        const q = query(collection(db, `ideas`), orderBy(sortBy, "desc"))
         const listenerUnsubscribe = onSnapshot(q, (snapshot) => {
             setIdeas(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as IdeaModel[])
         })
         return () => {
             listenerUnsubscribe()
         }
-    }, [])
+    }, [sortBy])
 
     return (
         <>
+            <Select placeholder='Sort by' onChange={(e) => setSortBy(e.target.value)}>
+                <option value='votes'>Votes</option>
+                <option value='createdDate'>Last added</option>
+            </Select>
+
             {ideas.map((idea) => {
                 return <Idea key={idea.id} id={idea.id} content={idea.content} createdDate={idea.createdDate} votes={idea.votes} />
             })}
